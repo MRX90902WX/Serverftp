1083d57fa747010b44b70857d5a553694e98c4f7
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-01-31 16:54:13 -0500

    Add files via upload

---
+++ ftpserver.py
@@ -0,0 +1,46 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+import os , socket
+from pyftpdlib.servers import FTPServer
+
+FTP_DIRECTORY = input("[+]Ingrese el directorio que desea exponer > ")
+FTP_PORT = 4444
+FTP_USER = 'admin'
+FTP_PASS = 'admin'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+IP = s.getsockname()[0]
+
+print(f'     ')
+print(f'                 Welcome of Hacker      ')
+print(f'                            ')
+print(f'                      ╱▔▔▔▔▔▔▔▔╲              ')
+print(f'                      ▏        ▕   ')
+print(f'                     ▕ ╭━╮┈┈╭━╮▕             ')
+print(f'                      ╲╰━╯╱╲╰━╯╱')
+print(f'            _   2022   ▏╮┈▔▔┈╭▕   By: MRX~HACKS ')
+print(f'           | \__________┣╋╋╋╋┫___________________')
+print(f'        @8@8< ___________________________________\ ')
+print(f'           |_/          ┣╋╋╋╋┫  ')
+print(f'                       ╲▂▂▂▂▂▂╱ ')
+print(f'      ')
+print(f'DIRECCION: ftp://{IP}:{FTP_PORT}')
+print(f'DIRECTORIO RAIZ: {FTP_DIRECTORY}')
+
+
+authorizer = DummyAuthorizer()
+authorizer.add_user(FTP_USER, FTP_PASS, FTP_DIRECTORY, perm='elradfmw')
+
+handler = FTPHandler
+handler.authorizer = authorizer
+handler.banner = 'Termux FTP Server :D'
+handler.passive_ports = range(50000, 55535)
+
+address = ('', FTP_PORT)
+server = FTPServer(address, handler)
+
+server.max_cons = 256
+server.max_cons_per_ip = 5
+
+server.serve_forever()
